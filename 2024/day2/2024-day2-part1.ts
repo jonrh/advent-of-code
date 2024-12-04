ea5c5391acbfd54d236670deba2aeb7a153cb293
Jón Rúnar Helgason <dev@jonrh.is>
2024-12-03 23:21:54 -0800

    Added comments

--- 2024/day2/2024-day2-part1.ts
+++ 2024/day2/2024-day2-part1.ts
@@ -11,12 +11,35 @@ const parseInput = (fileContents: string): Report[] => {
   });
 };
 
-const unsafeLevelChange = (a: Level, b: Level, asc: boolean) => {
+/**
+ * Returns true if a given change in two adjacent levels in a report is unsafe,
+ * true otherwise.
+ *
+ * Safe conditions:
+ *   - report is all in ascending or descending order
+ *   - the difference between levels can be 1, 2, or 3
+ *   - if the levels are the same the level change is unsafe
+ *
+ * It is assumed that level a precedes level b in the report list from left to
+ * right. Possible values for a and b in the report [1, 2, 3]: (1,2) and (2,3).
+ *
+ * @param a the first level in a report to compare to the second one
+ * @param b the second level in a report to compare to the first one
+ * @param asc if the report has been indicated to be in ascending order
+ */
+const unsafeLevelChange = (a: Level, b: Level, asc: boolean): boolean => {
   return asc
     ? (b - a <= 0 || b - a > 3)
     : (a - b <= 0 || a - b > 3);
 };
-const oneIfSafe = (report: Report) => isSafe(report) ? 1 : 0;
+
+/** Convenience function to sum number of safe reports */
+const oneIfSafe = (report: Report): 1 | 0 => isSafe(report) ? 1 : 0;
+
+/**
+ * Returns false at the first sign of an unsafe level change in a report, true
+ * otherwise if the report is safe.
+ */
 const isSafe = (report: Report): boolean => {
   const ascOrder = report[0] - report[1] < 0;
 
--- 2024/day2/readme.md
+++ 2024/day2/readme.md
@@ -4,7 +4,10 @@
   was structurally the same as the real data. Demo data had 5 numbers on 
   each line but real data could contain a lot more numbers. Very easy change 
   to account for but in the future it is likely best to take a peak at the 
-  real input data before working on the solution to avoid false assumptions. 
+  real input data before working on the solution to avoid false assumptions.
+- Not quite happy with the solution to part 1. It solved the problem in a 
+  fairly performant way but could not spot any patterns to simplify the code 
+  but suspect they do exist.
 
 ## Problem description, part 1
 --- Day 2: Red-Nosed Reports ---
